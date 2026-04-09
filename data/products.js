cad626f6245d17e7c11f370a364e5abd7729a6d8
SuperSimpleDev <timor090821@gmail.com>
2026-04-09 12:27:56 +0900

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -102,6 +102,8 @@ export function loadProductsFetch() {
       return new Product(productDetails);
     });
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
   return promise;
 }
@@ -124,6 +126,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
